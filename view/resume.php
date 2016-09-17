04c9f7429516cdee33dccead294c603e1bcd098a
Uldosphere <uldmail@gmail.com>
2016-09-17 14:53:02 +0200

    Remove Piwik
    Remove custom piwik tracker

--- view/resume.php
+++ view/resume.php
@@ -29,21 +29,6 @@
 			<script src="<?= VIEW_PATH; ?>js/html5shiv.js"></script>
 			<script src="<?= VIEW_PATH; ?>js/respond.min.js"></script>
 		<![endif]-->
-		<!-- Piwik -->
-		<script type="text/javascript">
-		  var _paq = _paq || [];
-		  _paq.push(['trackPageView']);
-		  _paq.push(['enableLinkTracking']);
-		  (function() {
-		    var u="//piwik.uldosphere.org/";
-		    _paq.push(['setTrackerUrl', u+'piwik.php']);
-		    _paq.push(['setSiteId', '1']);
-		    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-		    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
-		  })();
-		</script>
-		<noscript><p><img src="//piwik.uldosphere.org/piwik.php?idsite=1" style="border:0;" alt="" /></p></noscript>
-		<!-- End Piwik Code -->
 	</head>
 	<body data-spy="scroll" data-target="#navbar-example">	 
 		
